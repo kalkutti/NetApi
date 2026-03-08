3adc786be170c57601bc7be16ff6719c62c4c8ae
kalkutti <kalindu.sembakutti@gmail.com>
2026-03-08 10:18:51 -0400

    linked database to server

--- Program.cs
+++ Program.cs
@@ -18,11 +18,11 @@ builder.Services.AddScoped<IDressService, DressService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
